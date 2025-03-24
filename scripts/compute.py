1e888bcecc9b234209cc643e2ee62a92a4737929
katharina.starzer@eodc.eu <katharina.starzer@eodc.eu>
2025-03-24 13:59:01 +0100

    delete workshop files

--- scripts/compute.py
+++
@@ -1,6 +0,0 @@
-def sum_of_squares(n):
-    return sum(x**2 for x in range(n+1))
-
-if __name__ == "__main__":
-    result = sum_of_squares(50)
-    print(f"Computation completed: The sum of squares up to 50 is {result}")
