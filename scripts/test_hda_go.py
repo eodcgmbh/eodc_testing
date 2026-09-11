ec65d94b68dff7a49577d21ae584cde625f07d51
valentina <85164505+ValentinaHutter@users.noreply.github.com>
2026-09-11 12:54:12 +0200

    Update test_hda_go.py

--- scripts/test_hda_go.py
+++ scripts/test_hda_go.py
@@ -7,10 +7,10 @@ import pandas as pd
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 paths = ["https://data.eodc.eu/collections", "https://dev.hda.eodchosting.eu/collections"]
-sig0 = f"{paths[0]}/SENTINEL1_SIG0_20M/V1M2R3/EQUI7_EU020M/E048N015T3/SIG0_20260412T171426__VV_A015_E048N015T3_EU020M_V1M2R3_S1CIWGRDH_TUWIEN.tif"
-eopf = f"{paths[0]}/EOPF_ZARR/products/cpm_v300/S02MSIL2A/2026/09/06/S2B_MSIL2A_20260906T100019_N0512_R122_T35WNU_20260906T134813.zarr/zarr.json"
-sig0_dev = f"{paths[1]}/SENTINEL1_SIG0_20M/V1M2R3/EQUI7_EU020M/E048N015T3/SIG0_20260412T171426__VV_A015_E048N015T3_EU020M_V1M2R3_S1CIWGRDH_TUWIEN.tif"
-eopf_dev = f"{paths[1]}/EOPF_ZARR/products/cpm_v300/S02MSIL2A/2026/09/06/S2B_MSIL2A_20260906T100019_N0512_R122_T35WNU_20260906T134813.zarr/zarr.json"
+filepaths = []
+for path in paths:
+    filepaths.append(f"{path}/SENTINEL1_SIG0_20M/V1M2R3/EQUI7_EU020M/E048N015T3/SIG0_20260412T171426__VV_A015_E048N015T3_EU020M_V1M2R3_S1CIWGRDH_TUWIEN.tif")
+    filepaths.append(f"{path}/EOPF_ZARR/products/cpm_v300/S02MSIL2A/2026/09/06/S2B_MSIL2A_20260906T100019_N0512_R122_T35WNU_20260906T134813.zarr/zarr.json")
 
 def ok(resp):
     if not (200 <= resp.status_code < 300):
@@ -23,7 +23,7 @@ def main():
     try:
         msg = ""
         success = True
-        for filepath in [sig0, eopf, sig0_dev, eopf_dev]:
+        for filepath in filepaths:
             if success:
                 r = requests.get(filepath, timeout=15)
                 okc, msgc = ok(r)
